2e8649bb3fad49341b00e392d9e04bbe5486b226
Adam <admay202@gmail.com>
2025-01-02 16:57:53 -0600

    Initial Commit

---
+++ AniNet/AniNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniNetApp.swift
+//  AniNet
+//
+//  Created by Safiya May on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AniNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
